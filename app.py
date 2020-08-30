3ed21f9629cfea048c27ede3193f1007657aaa95
Narachii <naoya706oxford@gmail.com>
2020-08-30 17:21:12 +0900

    Fixed db initialization

--- app.py
+++ app.py
@@ -13,17 +13,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity) #/auth
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1/5000/student/Rolf
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
-
-if __name__ == '__main__':
-    db.init_app(app)
-    app.run(port=5000, debug=True)
