4af1aadc5579336d50bd8b2382a4292534e40223
Narachii <naoya706oxford@gmail.com>
2020-08-30 17:58:43 +0900

    Import os library

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
